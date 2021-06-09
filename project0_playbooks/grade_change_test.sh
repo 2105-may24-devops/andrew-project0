ca44db1dbf5e5be47cb61f6ca9a67c402b5b8945
AndrewWard2021 <andrewward22499@gmail.com>
2021-06-09 12:12:59 +0000

    adds test files

---
+++ project0_playbooks/grade_change_test.sh
@@ -0,0 +1,1 @@
+python3 -c 'import admin; grade_student("Test Student")'
---
+++ project0_playbooks/password_change_test.sh
@@ -0,0 +1,1 @@
+python3 -c 'import admin; admin.update_dict();  admin.change_studnent_password("Test Student","changedPass")'
