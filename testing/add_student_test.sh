38b702e7ac7990cfdd75df5bb0f7753b9532023e
AndrewWard2021 <andrewward22499@gmail.com>
2021-06-11 14:41:14 +0000

    test working with vms

--- testing/add_student_test.sh
+++ testing/add_student_test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 cd ~/ansible-project0/
-python -c 'import admin; admin.add_student("Test Student", 93)'
+python3 -c 'import admin; admin.add_student("Test Student", 93)'
 
-if [ -f "~/ansible-project0/students/Test_Student.txt" ]; then
+cd ./students
+if [ -f Test_Student.txt ]; then
 	echo "Test successful"
 else
 	echo "Test failed"
+	pwd
+	exit 1
 fi
--- testing/remove_student_test.sh
+++ testing/remove_student_test.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python3 -c 'import admin; admin.remove_student("Test Student")'
+cd ~/ansible-project0
+python3 -c 'import admin; admin.update_dict(); admin.remove_student("Test Student")'
+cd ./students
+if [ -f Test_Student.txt ]; then
+	echo "Test failed"
+	pwd
+	exit 1
+else
+	echo "Test Successful"
+fi
