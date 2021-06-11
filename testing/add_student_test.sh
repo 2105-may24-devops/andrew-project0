3b4b54c1c00964782c837d6cab09cef026367578
AndrewWard2021 <andrewward22499@gmail.com>
2021-06-11 12:56:18 +0000

    change to add student_test

--- testing/add_student_test.sh
+++ testing/add_student_test.sh
@@ -2,3 +2,9 @@
 
 cd ..
 python -c 'import admin; admin.add_student("Test Student", 93)'
+
+if [ -f "~/ansible-project0/students/Test_Student.txt" ]; then
+	echo "Test successful"
+else
+	echo "Test failed"
+fi
